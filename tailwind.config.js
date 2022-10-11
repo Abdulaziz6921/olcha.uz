2b36adffe4f0ec9bdd71efb7ce8f3710ddd678b0
Abdulaziz6921 <abdulaziztojibayev6@gmail.com>
2022-10-11 11:03:53 +0500

    first push

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      animation: {
+        beat: "beat 0.5s ease-out infinite",
+      },
+      keyframes: {
+        beat: {
+          "0%, 100%": { transform: "scale(1)" },
+          "50%": { transform: "scale(1.1)" },
+        },
+      },
+    },
+  },
+  plugins: [],
+};
